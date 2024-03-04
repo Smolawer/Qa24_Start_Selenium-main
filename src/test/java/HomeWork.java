065be6debf4e28bba138c9ff24257d4b0fa4eed6
Smolawer <simhawer@gmail.com>
2024-03-04 16:27:59 +0200

    44444

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -9,17 +10,18 @@ public class HomeWork {
     WebDriver wd;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/login");
     }
 
 
     @Test
-    public void cssLocators(){
+    public void cssLocators() {
 
         //by tag name
-        wd.findElement(By.tagName("a href"));
+        WebElement w1 = wd.findElement(By.tagName("a href"));
+        WebElement w2 = wd.findElement(By.tagName("a"));
 
 
         //by class
@@ -30,8 +32,5 @@ public class HomeWork {
         //by attribute
 
 
-        //one of elements find by attribute ==> start & end & contains value
-
     }
-
 }
