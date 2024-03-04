0c3943c68413451ef074a66846ccb377f041a3e3
Smolawer <simhawer@gmail.com>
2024-03-04 15:45:53 +0200

    1

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
@@ -18,6 +19,7 @@ public class HomeWork {
     public void cssLocators(){
 
         //by tag name
+        wd.findElement(By.tagName("a href"));
 
 
         //by class
